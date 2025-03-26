5662283eba772ddad4620a3c0cc88248bf0ed467
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2025-03-26 15:38:39 +0100

    Update a_propos.py

--- src/frontend/a_propos.py
+++ src/frontend/a_propos.py
@@ -1,10 +1,7 @@
 import streamlit as st
-from pathlib import Path
-import cv2
 
 def main():
     st.title("À propos")
-    image = cv2.imread(Path(__file__).parent.parent.parent / "ressources" / "tiny-imagenet-200/train/n07873807/images/n07873807_377.JPEG")
     st.write("Informations sur l'application et l'équipe de développement.")
     # Vérifier si la section "À propos" a déjà été chargée
     if "a_propos_loaded" not in st.session_state:
