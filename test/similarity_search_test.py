f8d1063a4d43e53ef917feddca61251364c2dc11
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2025-03-26 13:15:28 +0100

    Update similarity_search_test.py

--- test/similarity_search_test.py
+++ test/similarity_search_test.py
@@ -1,8 +1,9 @@
-import unittest
-from unittest.mock import patch, MagicMock
-import numpy as np
-from src.similarity_search import find_top5_categories, get_image_path
+""" Module de test unitaire pour la recherche de siilarité du fichier similarity_search.py. """
 
+import unittest, numpy as np
+from unittest.mock import patch
+from src.similarity_search import find_top5_categories, get_image_path
+from pathlib import Path
 
 class TestSimilaritySearch(unittest.TestCase):
     @classmethod
@@ -18,21 +19,21 @@ class TestSimilaritySearch(unittest.TestCase):
         ], dtype='float32')
 
         # Simuler des catégories Tiny ImageNet
-        cls.simulated_categories = np.array(['n01443537', 'n01629819', 'n01641577', 'n01644900', 'n01698640'])
+        cls.simulated_categories = np.array(['n01443537', 'n01629276', 'n01641577', 'n01644900', 'n01698640'])
 
         # Simuler le mapping WordNet
         cls.simulated_wordnet_mapping = {
-            'n01443537': 'goldfish',
-            'n01629819': 'salamander',
-            'n01641577': 'frog',
-            'n01644900': 'toad',
-            'n01698640': 'alligator'
+            'n01443537': 'goldfish, Carassius auratus',
+            'n01629276': 'salamander',
+            'n01641577': 'bullfrog, Rana catesbeiana',
+            'n01644900': 'tailed frog, bell toad, ribbed toad, tailed toad, Ascaphus trui',
+            'n01698640': 'American alligator, Alligator mississipiensis'
         }
 
-    @patch("src.similarity_search.tiny_imagenet_embeddings", new_callable=lambda: TestSimilaritySearch.simulated_embeddings)
-    @patch("src.similarity_search.tiny_imagenet_categories", new_callable=lambda: TestSimilaritySearch.simulated_categories)
-    @patch("src.similarity_search.wordnet_mapping", new_callable=lambda: TestSimilaritySearch.simulated_wordnet_mapping)
-    def test_find_top5_categories(self, mock_embeddings, mock_categories, mock_wordnet_mapping):
+    @patch("src.similarity_search.EMBEDDINGS_PATH", new_callable=lambda: TestSimilaritySearch.simulated_embeddings)
+    @patch("src.similarity_search.CATEGORIES_PATH", new_callable=lambda: TestSimilaritySearch.simulated_categories)
+
+    def test_find_top5_categories(self, mock_embeddings, mock_categories):
         """ Teste la fonction find_top5_categories avec un vecteur de caractéristiques simulé. """
         # Simuler un vecteur de caractéristiques
         query_features = np.array([0.15, 0.25, 0.35], dtype='float32')
@@ -52,18 +53,14 @@ class TestSimilaritySearch(unittest.TestCase):
         for category, _ in top5_categories:
             self.assertIn(category, self.simulated_wordnet_mapping.values())
 
-    @patch("src.similarity_search.tiny_imagenet_categories", new_callable=lambda: TestSimilaritySearch.simulated_categories)
-    def test_get_image_path(self, mock_categories):
+    @patch("src.similarity_search.CATEGORIES_PATH", new_callable=lambda: TestSimilaritySearch.simulated_categories)
+
+    def test_get_image_path(self,mock_categories):
         """ Teste la fonction get_image_path avec des indices simulés et un chemin de base simulé. """
-        # Index arbitraire pour le test
-        test_index = 2
 
-        # Chemin de base simulé
-        base_path = "test_dataset"
-
-        # Appeler la fonction à tester
-        image_path = get_image_path(test_index, base_path=base_path)
-
+        test_index = 2  # Index arbitraire pour le test
+        image_path = get_image_path(test_index)  # Appeler la fonction à tester
+        base_path = Path(__file__).parent.parent / "ressources" / "tiny-imagenet-200"
         # Construire le chemin attendu
         expected_class_id = self.simulated_categories[test_index]
         expected_image_name = f"{expected_class_id}_0.JPEG"
