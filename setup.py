41889c93abdb86fc641a0b4d17050f30dd52c6c4
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2025-03-26 10:05:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="projet",  # Nom du projet
     version="0.1.0",  # Version du projet
-    packages=find_packages(),  # Recherche automatiquement les packages
+    packages=find_packages(where='src'),  
+    package_dir={'': 'src'},
 )
