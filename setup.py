81505cffd56266cdfe45d702aea791bcb49afacc
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2025-04-02 10:08:06 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="projet",  # Nom du projet
-    version="0.1.0",  # Version du projet
-    packages=find_packages(where='src'),  
-    package_dir={'': 'src'},
-)
