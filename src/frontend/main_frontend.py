e50df3e08d79bb9857f9dfb19794946533f5c026
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2025-04-15 19:14:50 +0200

    Update main_frontend.py

--- src/frontend/main_frontend.py
+++ src/frontend/main_frontend.py
@@ -1,4 +1,12 @@
+""" Fichier gérant l'affichage général du site, avec un menu de navigation et un titre commun a toutes les pages. """
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 import streamlit as st
+
 from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="PixMatcher App", layout="wide")
